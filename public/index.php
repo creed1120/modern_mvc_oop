5a14f51551b4c1cf0e53b45b4fff547804f4fee9
creed1120 <cedricareed@gmail.com>
2022-12-03 14:35:15 -0700

    updated notes

--- public/index.php
+++ public/index.php
@@ -1,8 +1,11 @@
 <?php
+/*******************
+ * App entry point
+ *******************/
 
 session_start();
 
-// Loads all filed needed to run the application
+// Loads all files needed to run the application
 require('../app/core/init.php');
 
 $app = new App();
